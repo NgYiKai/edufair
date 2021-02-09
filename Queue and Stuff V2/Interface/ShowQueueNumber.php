01ecefb968282b9d12623dba87cf82ab7ee344d6
CBengChuan <73951355+CBengChuan@users.noreply.github.com>
2021-02-09 16:54:28 +0800

    Add files via upload
    Moved Insert Queue and Form.php to Database_Insert_Script
    Added Staff.php [Admin Interface without functions] 
    Added StaffStyle.css in design folder
    SQL Changes for Queue Table *Important
    CREATE TABLE Queue (
        Student_ID int(5),
        Queue_Num int(5),
        session TIMESTAMP,
        PRIMARY KEY(Queue_Num,Student_ID)
    )

---
+++ Queue and Stuff V2/Interface/ShowQueueNumber.php
@@ -0,0 +1,17 @@
+<?php
+    $Database_Name = "softwareeng";
+    $link = mysqli_connect("localhost", "root", "", $Database_Name);
+    $StudentID=$_POST['postStudID'];
+    $Query="SELECT queue.Queue_Num FROM queue WHERE Student_ID=$StudentID";
+    $query = $link->prepare($Query); // prepate a query
+    $query->execute(); // actually perform the query
+    $result = $query->get_result(); // retrieve the result so it can be used inside PHP
+    $r = $result->fetch_array(MYSQLI_ASSOC); // bind the data from the first result row to $r
+    //Storing Data From Personal_Info into variable
+    $QueueNum=$r['Queue_Num'];
+    if($QueueNum==null) {
+        include('../Database_Insert_Script/Insert_Queue.php');
+    }
+    echo $QueueNum;
+?>
+
