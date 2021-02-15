fb3f7728d6180f1056b00d261551a7ea085722f9
Ng Yi Kai <ykng0508@gmail.com>
2021-02-15 13:12:35 +0800

    Connection settings changed

--- config/database_connect.php
+++ config/database_connect.php
@@ -1,6 +1,6 @@
 <?php
     $Database_Name = "education fair";
-    $link = mysqli_connect("localhost", "abc123", "abc123", $Database_Name);
+    $link = mysqli_connect("localhost", "root", "", $Database_Name);
 
     if (!$link){
         echo 'Connection error:' . mysqli_connect_error();
